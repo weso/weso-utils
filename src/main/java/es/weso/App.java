d2eefb06db19856aa1578c9d1878fd18742e800b
César Luis Alvargonzález <sisa989@gmail.com>
2013-04-15 13:52:52 +0200

    First Commit, Empty Maven Project Includes autodeploy to Maven-Central Snapshots after successfull deploy (Travis-ci)

---
+++ src/main/java/es/weso/App.java
@@ -0,0 +1,13 @@
+package es.weso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
